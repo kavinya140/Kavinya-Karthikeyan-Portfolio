ce0bc4f52c2836fa530140707622abd3617372ff
Kavinya Karthikeyan <kavinyakarthi140@gmail.com>
2026-03-27 19:08:44 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Kavinya-Karthikeyan-Portfolio/",
-});
+  base: '/Kavinya-Karthikeyan-Portfolio/',
+  plugins: [react()],
+})
